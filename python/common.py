3e8c058c039b88bc840a3e8a19b4d58a0ba8185a
erolsson@kth.se <erolsson@kth.se>
2020-01-09 14:01:05 +0100

    "Adding post_processing in abaqus python"

--- python/common.py
+++ python/common.py
@@ -9,6 +9,7 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 subprocess.Popen('python find_numpy.py', shell=True)
 with open('python_modules.pkl') as module_pickle:
     modules = pickle.load(module_pickle)
+print modules
 numpy = imp.load_module('numpy', modules['numpy'][0], modules['numpy'][1], modules['numpy'][2])
 scipy = imp.load_module('scipy', modules['scipy'][0], modules['scipy'][1], modules['scipy'][2])
 print numpy.__version__, scipy.__version__
