e11bcf98dc31897e01c929cbba2364915a9e7773
erolsson@kth.se <erolsson@kth.se>
2020-01-09 12:46:11 +0100

    "Adding post_processing in abaqus python"

--- python/permanent_deformations.py
+++ python/permanent_deformations.py
@@ -8,8 +8,14 @@ import numpy as np
 from abaqus_functions.create_empty_odb import create_empty_odb
 from abaqus_functions.odb_io_functions import read_field_from_odb
 
+from material_model.functional_shapes import permanent_strain
+
 if __name__ == '__main__':
     cycles = 1e5
+
+    material_parameters = np.array([5.19502802e+00, 7.64481869e-07, 4.70091315e+01, 1.34948445e-13,
+                                    1.14929617e+01, 1.12439205e+01])
+
     simulation_directory = os.path.expanduser('~/railway_ballast/abaqus2014/')
     stress_odb_filename = simulation_directory + '/embankment_EO.odb'
     results_odb_filename = simulation_directory + '/results.odb'
@@ -23,5 +29,11 @@ if __name__ == '__main__':
     instance_names = stress_odb.rootAssembly.instances.keys()
     for instance_name in instance_names:
         stress = read_field_from_odb(field_id='S', odb_file_name=stress_odb_filename, instance_name=instance_name)
-        pressure = - np.sum(stress[:, 0:3], 1)
-        print(pressure.shape)
+        pressure = -np.sum(stress[:, 0:3], 1)
+        deviator = np.copy(stress)
+        deviator[:, 0:3] -= pressure
+        von_Mises = np.sqrt(np.sum(stress[:, 0:3]**2, 1)  - stress[:, 0]*stress[:, 1] -
+                            stress[:, 0]*stress[:, 2] - stress[:, 1]*stress[:, 2]+ 3*np.sum(stress[:, 3:]**2, 1))
+        direction = 1.2*deviator/von_Mises
+        ep = direction*permanent_strain(cycles, p=pressure, q=deviator, parameters=material_parameters)
+        print(np.max(np.max(ep, 1)))
