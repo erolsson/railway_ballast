60bd8a1c1a3256ce7307ae44143a1c68a2c3b6aa
erolsson@kth.se <erolsson@kth.se>
2020-01-09 13:25:17 +0100

    "Adding post_processing in abaqus python"

--- python/find_numpy.py
+++ python/find_numpy.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 
 import imp
+import os
 import pickle
 
+print(os.getcwd())
 python_modules = {name: imp.find_module(name) for name in ['numpy', 'scipy']}
 with open('python_modules.pkl', 'w') as pickle_handle:
     pickle.dump(python_modules, pickle_handle)
