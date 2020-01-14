8c9ae48ae101d78a56a7ec2c40a8ad7d1736d428
erolsson@kth.se <erolsson@kth.se>
2020-01-14 07:24:44 +0100

    "Adding post_processing in abaqus python"

--- python/material_model/functional_shapes.py
+++ python/material_model/functional_shapes.py
@@ -9,17 +9,6 @@ from common import scipy
 from scipy.integrate import solve_ivp
 from scipy.optimize import fmin
 
-import matplotlib.pyplot as plt
-import matplotlib.style
-
-matplotlib.style.use('classic')
-plt.rc('text', usetex=True)
-plt.rc('font', serif='Computer Modern Roman')
-plt.rcParams.update({'font.size': 20})
-plt.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
-plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman'],
-                  'monospace': ['Computer Modern Typewriter']})
-
 
 def permanent_strain(cycles, p, q, parameters):
     scalar = False
@@ -78,6 +67,16 @@ def residual(parameters, *args):
 
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import matplotlib.style
+
+    matplotlib.style.use('classic')
+    plt.rc('text', usetex=True)
+    plt.rc('font', serif='Computer Modern Roman')
+    plt.rcParams.update({'font.size': 20})
+    plt.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
+    plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman'],
+                      'monospace': ['Computer Modern Typewriter']})
     from experimental_results.experimental_results import sun_et_al_16
     fig = plt.figure(0)
     data = sun_et_al_16.get_data(f=5., p=[10, 30, 60])
