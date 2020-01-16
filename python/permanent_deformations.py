2a6e592f835e0f16274e283bd83842f8114c05e7
erolsson@kth.se <erolsson@kth.se>
2020-01-16 15:56:45 +0100

    "Adding post_processing in abaqus python"

--- python/permanent_deformations.py
+++ python/permanent_deformations.py
@@ -43,5 +43,5 @@ if __name__ == '__main__':
                 for j, gp in enumerate(C3D8.gauss_points):
                     B = element.B(*gp)
                     for comp in range(6):
-                        col[8*6*i+6*j:8*6*i+6*j+comp] = element.node_labels
+                        col[8*6*i+6*j+comp:8*6*i+6*j+comp+8] = element.node_labels
             print(col)
