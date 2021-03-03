b2916af79bc52b11283c76822467cbbef6a0c77e
erolsson@kth.se <erolsson@kth.se>
2021-03-03 17:36:22 +0100

    "Implementing scripted embankment model"

--- python/embankment_model/add_node_set.py
+++ python/embankment_model/add_node_set.py
@@ -6,15 +6,15 @@ def add_node_set_to_odb(odb_file_name, node_set_name, x_min=-1e99, x_max=1e99, y
                         z_min=-1e99, z_max=1e99, instance_name=None):
     odb = odbAccess.openOdb(odb_file_name, readOnly=False)
     if instance_name is None:
-        instance_name = odb.rootAssmebly.instances.keys()[0]
-    nodes = odb.rootAssmebly.instances[instance_name].nodes
+        instance_name = odb.rootAssembly.instances.keys()[0]
+    nodes = odb.rootAssembly.instances[instance_name].nodes
     set_node_labels = []
     for node in nodes:
         x, y, z = node.coordinates
         if x_min < x < x_max and y_min < y < y_max and z_min < z < z_max:
             set_node_labels.append(node.label)
 
-    odb.rootAssmebly.instances[instance_name].NodeSetFromNodeLabels(name=node_set_name, nodeLabels=set_node_labels)
+    odb.rootAssembly.instances[instance_name].NodeSetFromNodeLabels(name=node_set_name, nodeLabels=set_node_labels)
     odb.save()
     odb.close()
 
