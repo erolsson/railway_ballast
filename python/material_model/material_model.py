f6013649a303427a0fa0157456160dcbbab7385c
erolsson@kth.se <erolsson@kth.se>
2021-03-10 08:57:26 +0100

    "Implementing scripted embankment model"

--- python/material_model/material_model.py
+++ python/material_model/material_model.py
@@ -74,7 +74,6 @@ class MaterialModel:
             solution_comp = solve_ivp(dkv_dn, [n0, cycles[i]], [e_comp[0] + e_comp[1] + e_comp[2]])
 
             e_f = solution_fric.y[:, -1]
-            e_f[3:] *= 2                    # Calculating gamma_xy instead of e_xy etc
             e_c = solution_comp.y[:, -1]
             if np.linalg.norm(e_f) > 1.:
                 e_f /= np.linalg.norm(e_f)
@@ -90,7 +89,9 @@ class MaterialModel:
         return self.H1*(1 - np.exp(-self.nf*ep))
 
     def strain(self):
-        return self.frictional_strain - self.compaction_strain
+        e = self.frictional_strain - self.compaction_strain
+        e[:, 3:] *= 2
+        return e
 
     def volumetric_strain(self):
         e = self.strain()
@@ -98,6 +99,7 @@ class MaterialModel:
 
     def deviatoric_strain(self):
         e_dev = np.array(self.strain())
+        e_dev[:, 3:] *= 2
         e_vol = self.volumetric_strain()
         for i in range(3):
             e_dev[:, i] -= e_vol/3
