aff1300cae58315528956a2799ee3696d3fa7754
erolsson@kth.se <erolsson@kth.se>
2021-03-15 12:50:32 +0100

    "Implementing scripted embankment model"

--- python/plot_stresses_along_path.py
+++ python/plot_stresses_along_path.py
@@ -41,12 +41,13 @@ def main():
             plt.figure(0)
             plt.plot(path_points[0, 1] - path_points[:, 1], static_pressure/1e3, 'k' + line, lw=2)
             for load, c in zip([22.5, 30.], ['r', 'b']):
-                step_name = 'cyclic_stresses_' + str(load).replace('.', '_') + 't'
-                s = get_stress_tensor_from_path(odb_filename, path_points, step_name=step_name)
-                von_mises = (np.sum(s[:, :3]**2, axis=1) + 3*np.sum(s[:, 3:]**2, axis=1)
-                             - s[:, 0]*s[:, 1] - s[:, 0]*s[:, 2] - s[:, 1]*s[:, 2])**0.5
-                plt.figure(1)
-                plt.plot(path_points[0, 1] - path_points[:, 1], von_mises/1e3, c + line, lw=2)
+                if load != 30. and rail_fixture != 'sleepers':
+                    step_name = 'cyclic_stresses_' + str(load).replace('.', '_') + 't'
+                    s = get_stress_tensor_from_path(odb_filename, path_points, step_name=step_name)
+                    von_mises = (np.sum(s[:, :3]**2, axis=1) + 3*np.sum(s[:, 3:]**2, axis=1)
+                                 - s[:, 0]*s[:, 1] - s[:, 0]*s[:, 2] - s[:, 1]*s[:, 2])**0.5
+                    plt.figure(1)
+                    plt.plot(path_points[0, 1] - path_points[:, 1], von_mises/1e3, c + line, lw=2)
         name = rail_fixture[0].upper() + rail_fixture[1:]
         plt.figure(0)
         plt.plot([0, -1], [-1, -1], 'k' + line, lw=2, label=name)
